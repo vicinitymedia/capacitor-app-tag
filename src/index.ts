db5751c53a9c759844adce02eb86b2d4692a4208
Gam13l <riedaa@vicinity-media.com>
2023-02-03 11:56:29 +0200

    file format linting applied for code styling consistency

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { VicinityMediaTagPlugin } from './definitions';
 
-const VicinityMediaTag = registerPlugin<VicinityMediaTagPlugin>('VicinityMediaTag', {
-  web: () => import('./web').then(m => new m.VicinityMediaTagWeb()),
-});
+const VicinityMediaTag = registerPlugin<VicinityMediaTagPlugin>(
+  'VicinityMediaTag',
+  {
+    web: () => import('./web').then(m => new m.VicinityMediaTagWeb()),
+  },
+);
 
 export * from './definitions';
 export { VicinityMediaTag };
